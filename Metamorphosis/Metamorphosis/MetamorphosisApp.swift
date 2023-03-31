416934cf8385c0e045d48ab5832fd43e312cd85b
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2023-03-31 23:52:39 +0300

    [add] base xcode project

---
+++ Metamorphosis/Metamorphosis/MetamorphosisApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetamorphosisApp.swift
+//  Metamorphosis
+//
+//  Created by Vladyslav Shkodych on 31.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct MetamorphosisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
