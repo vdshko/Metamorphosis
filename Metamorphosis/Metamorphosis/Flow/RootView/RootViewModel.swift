40570dfb0c38363c6db9a4bdd5d36b90c2c42894
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2023-04-02 23:21:26 +0300

    [add] RootView to the flow

---
+++ Metamorphosis/Metamorphosis/Flow/RootView/RootViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  RootViewModel.swift
+//  Metamorphosis
+//
+//  Created by Vladyslav Shkodych on 02.04.2023.
+//
+
+import SwiftUI
+
+extension RootView {
+    
+    final class RootViewModel: ObservableObject {
+        
+        init() {
+            setupBinding()
+        }
+    }
+}
+
+private extension RootView.RootViewModel {
+    
+    func setupBinding() {}
+}
