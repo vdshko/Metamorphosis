3dee6d0eac97d2146c2470580cde66b7a89a860f
Vladyslav Shkodych <vladyslav.shkodych@gmail.com>
2023-04-04 13:35:31 +0300

    [add] units and grid UI

--- Metamorphosis/Metamorphosis/Resources/SwiftGen/Assets.swift
+++ Metamorphosis/Metamorphosis/Resources/SwiftGen/Assets.swift
@@ -13,15 +13,33 @@ import UIKit
 public enum Asset {
   public enum Assets {
     public static let launchScreen = ImageAsset(name: "LaunchScreen")
+    public enum Units {
+      public static let currency = ImageAsset(name: "Units/Currency")
+      public static let angles = ImageAsset(name: "Units/angles")
+      public static let area = ImageAsset(name: "Units/area")
+      public static let data = ImageAsset(name: "Units/data")
+      public static let length = ImageAsset(name: "Units/length")
+      public static let power = ImageAsset(name: "Units/power")
+      public static let pressure = ImageAsset(name: "Units/pressure")
+      public static let speed = ImageAsset(name: "Units/speed")
+      public static let temperature = ImageAsset(name: "Units/temperature")
+      public static let time = ImageAsset(name: "Units/time")
+      public static let volume = ImageAsset(name: "Units/volume")
+      public static let weight = ImageAsset(name: "Units/weight")
+    }
   }
   public enum Colors {
+    public enum Base {
+      public static let black = ColorAsset(name: "Base/black")
+      public static let white = ColorAsset(name: "Base/white")
+    }
   }
 }
 // swiftlint:enable identifier_name line_length nesting type_body_length type_name
 
 // MARK: - Implementation Details
 
-public struct ColorAsset {
+public struct ColorAsset: Hashable {
   fileprivate let name: String
 
   public var color: Color {
@@ -61,7 +79,7 @@ extension Text {
     }
 }
 
-public struct ImageAsset {
+public struct ImageAsset: Hashable {
   fileprivate let name: String
 
   public var image: Image {
